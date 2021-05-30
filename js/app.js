1612ece9cac0142f9cdbed3d0af37d7d72224489
ManucaCh <manuchaves@gmail.com>
2021-05-30 00:46:34 -0300

    add error on 404 alert & basic css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 async function fetchData() {
-  const inputValue = document.getElementById("pokemonSearch").value;
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
-  const json = await data.json();
-  //   console.log(typeof data);
+  const inputValue = document.getElementById("searchInput").value;
+  try {
+    // console.log(typeof data);
 
-  if (0 < inputValue && inputValue < 898) {
-    document.getElementById("pokemonResult").innerText = json.name;
-    document.getElementById("pokemonImage").src = json.sprites.front_default;
-  } else {
-    alert("Pokemon does not exist");
+    if (0 < inputValue && inputValue < 899) {
+      const data = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${inputValue}`
+      );
+      const json = await data.json();
+      document.getElementById("pokemonResult").innerText = json.name;
+      document.getElementById(
+        "pokemonImage"
+      ).src = `https://img.pokemondb.net/artwork/large/${json.name}.jpg`;
+    } else {
+      alert("Pokemon does not exist");
+    }
+  } catch (err) {
+    console.log(err);
   }
+  clearInput();
 }
 
 function enterInput(e) {
   if (e.keyCode == 13) {
     fetchData();
+    clearInput();
   }
+}
+
+function clearInput() {
+  document.getElementById("searchInput").value = "";
 }
