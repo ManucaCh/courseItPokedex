154997936a1d71f5fecb6b2daa9cd45dbf740b42
ManucaCh <manuchaves@gmail.com>
2021-05-31 15:52:53 -0300

    add pkmn search by name

--- js/app.js
+++ js/app.js
@@ -3,28 +3,22 @@ async function fetchData() {
   try {
     // console.log(typeof data);
 
-    if (0 < inputValue && inputValue < 899) {
-      const data = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${inputValue}`
-      );
-      const json = await data.json();
-      document.getElementById("pokemonResult").innerText = json.name;
-      document.getElementById("pokemonID").innerText = json.id;
-      document.getElementById("pokemonType1").innerText =
-        json.types[0].type.name;
-      document.getElementById(
-        "pokemonImage"
-      ).src = `https://img.pokemondb.net/artwork/large/${json.name}.jpg`;
-      if (json.types.length == 2) {
-        document.getElementById("pokemonType2").innerText =
-          json.types[1].type.name;
-      } else {
-        document.getElementById("pokemonType2").innerText = "---";
-      }
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+    const json = await data.json();
+    document.getElementById("pokemonResult").innerText = json.name;
+    document.getElementById("pokemonID").innerText = json.id;
+    document.getElementById("pokemonType1").innerText = json.types[0].type.name;
+    document.getElementById(
+      "pokemonImage"
+    ).src = `https://img.pokemondb.net/artwork/large/${json.name}.jpg`;
+    if (json.types.length == 2) {
+      document.getElementById("pokemonType2").innerText =
+        json.types[1].type.name;
     } else {
-      alert("Pokemon does not exist");
+      document.getElementById("pokemonType2").innerText = "---";
     }
   } catch (err) {
+    alert("Pokemon does not exist");
     console.log(err);
   }
   clearInput();
