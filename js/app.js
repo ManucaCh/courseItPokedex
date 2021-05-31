63498a4e8ee312cc18a428a723ff5d7504d8dfb0
ManucaCh <manuchaves@gmail.com>
2021-05-31 15:31:00 -0300

    add css to result screen & fetch pkmn types

--- js/app.js
+++ js/app.js
@@ -9,9 +9,18 @@ async function fetchData() {
       );
       const json = await data.json();
       document.getElementById("pokemonResult").innerText = json.name;
+      document.getElementById("pokemonID").innerText = json.id;
+      document.getElementById("pokemonType1").innerText =
+        json.types[0].type.name;
       document.getElementById(
         "pokemonImage"
       ).src = `https://img.pokemondb.net/artwork/large/${json.name}.jpg`;
+      if (json.types.length == 2) {
+        document.getElementById("pokemonType2").innerText =
+          json.types[1].type.name;
+      } else {
+        document.getElementById("pokemonType2").innerText = "---";
+      }
     } else {
       alert("Pokemon does not exist");
     }
